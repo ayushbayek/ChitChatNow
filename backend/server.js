b4b5d74bbea9cef99aeb3080644aabedd012b34d
Ayush Ranjan <ayush81029@gmail.com>
2023-08-01 16:39:34 +0530

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import { chats } from "./data/data.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Setting cors for all sites
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.listen(PORT, console.log(`Server is working ${PORT}`));
